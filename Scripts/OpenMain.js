f764892b749b7d9db00f7a550107b19ae2169407
themightdodo <98085659+themightdodo@users.noreply.github.com>
2024-11-04 13:29:56 +0100

    Website!!

---
+++ Scripts/OpenMain.js
@@ -0,0 +1,6 @@
+let body = document.querySelector("body");
+
+
+body.classList.remove("bg-blanc2");
+body.classList.add("bg-blanc");
+    
---
+++ Scripts/Transi.js
@@ -0,0 +1,5 @@
+let Enter = document.querySelector(".transi-enter");
+
+
+    Enter.classList.remove("h-[100vh]");
+    Enter.style.height = "0px";
