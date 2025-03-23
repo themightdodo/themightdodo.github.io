bea7a1252bd819698349fc76d5236b8a55c4cbb4
themightdodo <98085659+themightdodo@users.noreply.github.com>
2025-03-23 17:32:32 +0100

    reload on page history

--- Scripts/Transi.js
+++ Scripts/Transi.js
@@ -3,3 +3,12 @@ let Enter = document.querySelector(".transi-enter");
 
     Enter.classList.remove("h-[100vh]");
     Enter.style.height = "0px";
+    window.addEventListener( "pageshow", function ( event ) {
+        var historyTraversal = event.persisted || 
+                               ( typeof window.performance != "undefined" && 
+                                    window.performance.navigation.type === 2 );
+        if ( historyTraversal ) {
+          // Handle page restore.
+          window.location.reload();
+        }
+      });
